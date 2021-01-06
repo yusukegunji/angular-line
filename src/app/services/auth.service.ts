0941764a4a77564644c5352f512874309f9c61de
Yusuke Gunji <inochideutae@gmail.com>
2021-01-06 17:55:02 +0900

    feat(auth.service): user$追加

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,18 +1,36 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireFunctions } from '@angular/fire/functions';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { shareReplay, switchMap } from 'rxjs/operators';
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  user: any;
+  user$: Observable<User> = this.afAuth.user.pipe(
+    switchMap((user) => {
+      this.user = user;
+      if (user) {
+        return this.db.doc(`users/${user.uid}`).valueChanges();
+      } else {
+        return of(null);
+      }
+    }),
+    shareReplay(1)
+  );
+
   constructor(
     private fns: AngularFireFunctions,
     private afAuth: AngularFireAuth,
     private snackbar: MatSnackBar,
-    private router: Router
+    private router: Router,
+    private db: AngularFirestore
   ) {}
 
   async loginWithLine(code: string): Promise<void> {
