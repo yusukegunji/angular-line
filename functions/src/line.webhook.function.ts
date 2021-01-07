cc4925ac9ae3b2052646f2efc38320e888a4ae31
Yusuke Gunji <inochideutae@gmail.com>
2021-01-07 23:14:42 +0900

    feat(fns): line.webhook追加

---
+++ functions/src/line.webhook.function.ts
@@ -0,0 +1,22 @@
+import * as functions from 'firebase-functions';
+import { LineClient } from 'messaging-api-line';
+
+const client = new LineClient({
+  accessToken: functions.config().line.message_token,
+  channelSecret: functions.config().line.message_secret,
+});
+
+export const lineWebhook = functions.https.onRequest(
+  async (request, response) => {
+    const event = request.body.events[0];
+
+    if (event.type === 'message') {
+      client.reply(event.replyToken, [
+        {
+          type: 'text',
+          text: 'test',
+        },
+      ]);
+    }
+  }
+);
