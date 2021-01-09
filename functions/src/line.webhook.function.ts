258cb599be8f7599fbde8b577d1ab7e47fa92404
Yusuke Gunji <inochideutae@gmail.com>
2021-01-09 22:07:26 +0900

    feat(fns): change message and add import

--- functions/src/line.webhook.function.ts
+++ functions/src/line.webhook.function.ts
@@ -1,5 +1,8 @@
 import * as functions from 'firebase-functions';
 import { LineClient } from 'messaging-api-line';
+import * as admin from 'firebase-admin';
+
+export const db = admin.firestore();
 
 const client = new LineClient({
   accessToken: functions.config().line.message_token,
@@ -8,14 +11,14 @@ const client = new LineClient({
 
 export const lineWebhook = functions
   .region('asia-northeast1')
-  .https.onRequest(async (request, response) => {
-    const event = request.body.events[0];
+  .https.onRequest(async (req, res) => {
+    const event = req.body.events[0];
 
     if (event.type === 'message') {
       await client.reply(event.replyToken, [
         {
           type: 'text',
-          text: 'test',
+          text: 'メッセージありがとう',
         },
       ]);
     }
