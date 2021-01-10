a91101513fe8e71e891a7c2247693005416f8e98
Yusuke Gunji <inochideutae@gmail.com>
2021-01-10 23:31:51 +0900

    feat(member-card): add member-card

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
 import { MatButtonModule } from '@angular/material/button';
+import { MemberCardComponent } from './member-card/member-card.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, MemberCardComponent],
   imports: [CommonModule, HomeRoutingModule, MatButtonModule],
 })
 export class HomeModule {}
