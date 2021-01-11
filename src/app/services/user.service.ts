b29d03fabdb004f60fb392a4c4d324e36eb44b09
Yusuke Gunji <inochideutae@gmail.com>
2021-01-12 03:44:06 +0900

    feat(user.service): add joinTeam

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  constructor(private db: AngularFirestore) {}
 
-  constructor() { }
+  joinTeam(teamId: string, uid: string): void {
+    this.db.doc(`teams/${teamId}/joinedUids/${uid}`).set({
+      teamId,
+      uid,
+    });
+    this.db.doc(`users/${uid}/joinedEvents/${teamId}`).set({ teamId });
+  }
 }
