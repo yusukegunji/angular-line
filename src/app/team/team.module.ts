7b1e40121274fd18fbfd771dab395dfc4aa41d45
Yusuke Gunji <inochideutae@gmail.com>
2021-01-12 03:45:13 +0900

    feat(team): add settings menu btn

--- src/app/team/team.module.ts
+++ src/app/team/team.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { TeamRoutingModule } from './team-routing.module';
 import { TeamComponent } from './team/team.component';
 import { MemberCardComponent } from '../member-card/member-card.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [TeamComponent, MemberCardComponent],
-  imports: [CommonModule, TeamRoutingModule],
+  imports: [CommonModule, TeamRoutingModule, MatMenuModule, MatIconModule],
 })
 export class TeamModule {}
