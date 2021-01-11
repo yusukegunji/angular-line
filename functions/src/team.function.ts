c5ba110311a5002fb10c9c15d0efc24e22662bef
Yusuke Gunji <inochideutae@gmail.com>
2021-01-12 05:59:49 +0900

    fix(fns): fix judgementPassword

--- functions/src/team.function.ts
+++ functions/src/team.function.ts
@@ -6,8 +6,8 @@ const db = admin.firestore();
 export const judgementPassword = functions
   .region('asia-northeast1')
   .https.onCall(async (data: any) => {
-    const team = (await db.doc(`private/${data.teamId}`).get()).data() as any;
-    const password = team.password;
+    const team = (await db.doc(`private/${data.teamId}`).get()).data();
+    const password = team?.password;
     try {
       if (data.password === password) {
         return true;
