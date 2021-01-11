1bed5a23d98e71f1f75dc14a3d4f97d917b45121
Yusuke Gunji <inochideutae@gmail.com>
2021-01-12 03:38:27 +0900

    feat(header): add openJoinTeamDialog

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { JoinTeamDialogComponent } from '../join-team-dialog/join-team-dialog.component';
 import { AuthService } from '../services/auth.service';
 
 @Component({
@@ -7,9 +9,19 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService, private dialog: MatDialog) {}
 
   ngOnInit(): void {}
+
+  openJoinTeamDialog(uid: string): void {
+    this.dialog.open(JoinTeamDialogComponent, {
+      maxWidth: '100vw',
+      minWidth: '50%',
+      autoFocus: false,
+      restoreFocus: false,
+      data: { uid },
+    });
+  }
 
   logout(): void {
     this.authService.logout();
