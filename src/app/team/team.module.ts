a2e1ba08109c8c1236c7423ceb54448ca772c1c2
Yusuke Gunji <inochideutae@gmail.com>
2021-01-12 06:06:51 +0900

    feat(team): add delete function and UI

--- src/app/team/team.module.ts
+++ src/app/team/team.module.ts
@@ -6,9 +6,16 @@ import { TeamComponent } from './team/team.component';
 import { MemberCardComponent } from '../member-card/member-card.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
+import { DialogsModule } from '../dialogs/dialogs.module';
 
 @NgModule({
   declarations: [TeamComponent, MemberCardComponent],
-  imports: [CommonModule, TeamRoutingModule, MatMenuModule, MatIconModule],
+  imports: [
+    CommonModule,
+    TeamRoutingModule,
+    MatMenuModule,
+    MatIconModule,
+    DialogsModule,
+  ],
 })
 export class TeamModule {}
