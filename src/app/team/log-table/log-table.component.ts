8ea7d2b13c7f2a1c65bec5a1909a3992a2bff31f
Yusuke Gunji <inochideutae@gmail.com>
2021-01-15 05:39:14 +0900

    feat(log-table): add team$ and DataSource

--- src/app/team/log-table/log-table.component.ts
+++ src/app/team/log-table/log-table.component.ts
@@ -7,8 +7,13 @@ import {
 } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
-import { Log } from 'src/app/interfaces/log';
+import { ActivatedRoute } from '@angular/router';
+import { StickerMessage } from 'functions/node_modules/messaging-api-line/dist/LineTypes';
+import { Observable } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
+import { Log, LogWithUser } from 'src/app/interfaces/log';
 import { Team } from 'src/app/interfaces/team';
+import { LogService } from 'src/app/services/log.service';
 import { TeamService } from 'src/app/services/team.service';
 
 @Component({
@@ -19,32 +24,30 @@ import { TeamService } from 'src/app/services/team.service';
 export class LogTableComponent implements OnInit, AfterViewInit {
   readonly displayedColumns: string[] = [
     'status',
+    'photoURL',
     'name',
     'position',
     'logedInAt',
     'tookBreakAt',
     'backedBreakAt',
     'logedOutAt',
+    'sum',
     'location',
     'commutingFee',
     'menu',
   ];
 
-  dataSource = [
-    {
-      status: 'string',
-      name: 'string',
-      position: 'string',
-      logedInAt: { seconds: 1610393765, nanoseconds: 75000000 },
-      tookBreakAt: { seconds: 1610393765, nanoseconds: 75000000 },
-      backedBreakAt: { seconds: 1610393765, nanoseconds: 75000000 },
-      logedOutAt: { seconds: 1610393765, nanoseconds: 75000000 },
-      location: 'string',
-      commutingFee: 1000,
-    },
-  ];
-
+  dataSource: MatTableDataSource<LogWithUser>;
   defaultPageSize = 10;
+  isLoading: boolean;
+  teamId: string;
+
+  team$: Observable<Team> = this.route.paramMap.pipe(
+    switchMap((param) => {
+      const teamId = param.get('id');
+      return this.teamService.getTeam(teamId);
+    })
+  );
 
   @Input() team: Team;
 
@@ -55,11 +58,20 @@ export class LogTableComponent implements OnInit, AfterViewInit {
     }
   }
 
-  constructor(private teamService: TeamService) {}
+  constructor(
+    private teamService: TeamService,
+    private logService: LogService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {}
-
-  ngAfterViewInit(): void {
-    this.dataSource.paginator = this.paginator;
+  ngOnInit(): void {
+    this.team$.subscribe((team) => {
+      this.teamId = team.teamId;
+      this.logService.getLogsWithUser(this.teamId).subscribe((data) => {
+        this.dataSource = new MatTableDataSource<LogWithUser>(data);
+      });
+    });
   }
+
+  ngAfterViewInit(): void {}
 }
