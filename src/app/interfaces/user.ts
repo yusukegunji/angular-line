45e4ed600e8d0fb5ddad6c77a4e2e260df321dee
Yusuke Gunji <inochideutae@gmail.com>
2021-01-20 21:48:44 +0900

    feat(user.ts): add property

--- src/app/interfaces/user.ts
+++ src/app/interfaces/user.ts
@@ -3,10 +3,10 @@ export interface User {
   name: string;
   email: string;
   photoURL: string;
-  creanCount?: number;
   isAdmin?: boolean;
   post?: string;
   result?: number;
   plan?: number;
   createdAt: firebase.default.firestore.Timestamp;
+  isWorking: boolean;
 }
