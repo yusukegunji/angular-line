713282dd795efd153bbadfc022113ed2b20aceb8
Yusuke Gunji <inochideutae@gmail.com>
2021-01-22 01:36:12 +0900

    feat(welcome): remove login-component

--- src/app/welcome/welcome-routing.module.ts
+++ src/app/welcome/welcome-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
   {
     path: '',
     component: WelcomeComponent,
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
   },
 ];
 
