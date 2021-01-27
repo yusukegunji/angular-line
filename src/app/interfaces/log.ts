c0f86762ea836f13c3a438c635c0329339308aae
Yusuke Gunji <inochideutae@gmail.com>
2021-01-27 21:52:59 +0900

    fix(interfaces): fix log.ts

--- src/app/interfaces/log.ts
+++ src/app/interfaces/log.ts
@@ -1,17 +1,18 @@
 import { User } from './user';
 
 export interface Log {
-  status: string;
   name: string;
-  position: string;
-  logedInAt: firebase.default.firestore.Timestamp;
-  tookBreakAt: firebase.default.firestore.Timestamp;
-  backedBreakAt: firebase.default.firestore.Timestamp;
-  logedOutAt: firebase.default.firestore.Timestamp;
-  location: firebase.default.firestore.GeoPoint;
-  commutingFee: number;
-  uid: string;
+  position?: string;
+  isWorking: boolean;
+  logedInAt?: firebase.default.firestore.Timestamp;
+  tookBreakAt?: firebase.default.firestore.Timestamp;
+  backedBreakAt?: firebase.default.firestore.Timestamp;
+  logedOutAt?: firebase.default.firestore.Timestamp;
+  location?: firebase.default.firestore.GeoPoint;
+  commutingFee?: number;
+  userId: string;
   teamId: string;
+  logId: string;
 }
 
 export interface LogWithUser extends Log {
