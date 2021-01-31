7a2b3a291d1c07802f6893883799b99575e43ed3
Yusuke Gunji <inochideutae@gmail.com>
2021-01-31 20:06:28 +0900

    feat(log-table): add zero fill & breakTime=0 fnc

--- src/app/team/log-table/log-table.component.ts
+++ src/app/team/log-table/log-table.component.ts
@@ -9,7 +9,7 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import { LogWithUser } from 'src/app/interfaces/log';
 import { Team } from 'src/app/interfaces/team';
 import { LogService } from 'src/app/services/log.service';
@@ -21,6 +21,9 @@ import { TeamService } from 'src/app/services/team.service';
   styleUrls: ['./log-table.component.scss'],
 })
 export class LogTableComponent implements OnInit, AfterViewInit {
+  @Input() team: Team;
+  @Input() monthId: string;
+
   readonly displayedColumns: string[] = [
     'status',
     'photoURL',
@@ -33,8 +36,8 @@ export class LogTableComponent implements OnInit, AfterViewInit {
     'logedOutAt',
     'totalTime',
     'overTime',
-    'location',
-    'commutingFee',
+    // 'location',
+    // 'commutingFee',
     'menu',
   ];
 
@@ -47,21 +50,20 @@ export class LogTableComponent implements OnInit, AfterViewInit {
 
   defaultPageSize = 10;
   isLoading: boolean;
-  teamId: string;
   totalTime: any;
   totalWorkTime: any;
   totalBreakTime: any;
   roundTime: any;
   overTime: any;
+  breakTime: number;
   plan = 28800000;
+
   team$: Observable<Team> = this.route.paramMap.pipe(
     switchMap((param) => {
       const teamId = param.get('id');
       return this.teamService.getTeam(teamId);
     })
   );
-
-  @Input() team: Team;
 
   @ViewChild(MatPaginator)
   set paginator(value: MatPaginator) {
@@ -78,23 +80,26 @@ export class LogTableComponent implements OnInit, AfterViewInit {
 
   ngOnInit(): void {
     this.logService
-      .getDailyLogsWithUser(this.team.teamId, 'AECxI0VA3ko21z19THh3')
+      .getDailyLogsWithUser(this.team.teamId, this.monthId)
       .subscribe((logsWithUser) => {
         this.dataSource.data = logsWithUser.map((log: LogWithUser) => {
           const breakIn: any = log.tookBreakAt?.toDate();
           const breakOut: any = log.backedBreakAt?.toDate();
-          const breakTime = 1000 * Math.round((breakOut - breakIn) / 1000);
-          const bt = new Date(breakTime);
+          this.breakTime =
+            !log.tookBreakAt || !log.backedBreakAt
+              ? 0
+              : 1000 * Math.round((breakOut - breakIn) / 1000);
+          const bt = new Date(this.breakTime);
           this.totalBreakTime = bt.getUTCHours() + ':' + bt.getUTCMinutes();
 
           const logOut: any = log.logedOutAt?.toDate();
           const logIn: any = log.logedInAt?.toDate();
           const workTime =
-            1000 * Math.round((logOut - logIn - breakTime) / 1000);
+            1000 * Math.round((logOut - logIn - this.breakTime) / 1000);
           const wt = new Date(workTime);
           this.totalWorkTime = wt.getUTCHours() + ':' + wt.getUTCMinutes();
 
-          const resultTime = workTime - breakTime - this.plan;
+          const resultTime = workTime - this.breakTime - this.plan;
           const rt = new Date(resultTime);
           this.overTime =
             resultTime > 0 ? rt.getUTCHours() + ':' + rt.getUTCMinutes() : 0;
