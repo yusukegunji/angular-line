08bfb0392dc046bb4a880bc8d95c958006e05e6a
Yusuke Gunji <inochideutae@gmail.com>
2021-01-31 19:53:16 +0900

    feat(team-routing): add monthId

--- src/app/team/team-routing.module.ts
+++ src/app/team/team-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LogTableComponent } from './log-table/log-table.component';
 import { TeamComponent } from './team/team.component';
 
 const routes: Routes = [
   {
     path: ':id',
     component: TeamComponent,
+    children: [
+      {
+        path: ':monthId',
+        component: LogTableComponent,
+      },
+    ],
   },
 ];
 
