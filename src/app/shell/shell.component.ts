f6e29e012294f9f273cf4a8cf9497e2f4bd34c73
Yusuke Gunji <inochideutae@gmail.com>
2021-02-09 23:46:42 +0900

    feat(shell): add side-nav

--- src/app/shell/shell.component.ts
+++ src/app/shell/shell.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UiService } from '../services/ui.service';
 
 @Component({
   selector: 'app-shell',
   templateUrl: './shell.component.html',
-  styleUrls: ['./shell.component.scss']
+  styleUrls: ['./shell.component.scss'],
 })
 export class ShellComponent implements OnInit {
+  opened$: Observable<boolean> = this.uiService.isOpen$;
+  scrWidth: any;
 
-  constructor() { }
+  constructor(private uiService: UiService) {}
 
   ngOnInit(): void {
+    this.getScreenSize();
   }
 
+  toggleNav(): void {
+    this.uiService.toggleOpening();
+    console.log('toggle');
+  }
+
+  getScreenSize(): void {
+    this.scrWidth = window.innerWidth;
+  }
 }
