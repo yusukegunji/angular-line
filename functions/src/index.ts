94951512a007834d540d661fa96465c3aa7380c7
Yusuke Gunji <inochideutae@gmail.com>
2021-02-14 22:31:22 +0900

    feat(agora.fun): add agora.func

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,6 +5,7 @@ admin.initializeApp(functions.config().firebase);
 
 export * from './auth.function';
 export * from './user.function';
+export * from './agora.function';
 export { lineMsgApi } from './line.msg.function';
 export { judgementPassword } from './team.function';
 export { countUpMember, countDownMember } from './join.event.function';
