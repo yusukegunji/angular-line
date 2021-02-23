dd7837a057b9586cf78b6713b39320210055ce51
Yusuke Gunji <inochideutae@gmail.com>
2021-02-23 21:50:28 +0900

    feat(chennel): add meeting.serv

--- src/app/meeting/channel/channel.component.ts
+++ src/app/meeting/channel/channel.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
+import AgoraRTC from 'agora-rtc-sdk-ng';
 import { Observable } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 import { User } from 'src/app/interfaces/user';
@@ -12,12 +14,15 @@ import { MeetingService } from 'src/app/services/meeting.service';
   styleUrls: ['./channel.component.scss'],
 })
 export class ChannelComponent implements OnInit {
+  client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
+
   user$: Observable<User> = this.authService.user$;
   channelId$: Observable<string> = this.route.paramMap.pipe(
     map((params) => params.get('channelId'))
   );
   channelId: string;
   players: any;
+  isProcessing: boolean;
 
   participants$: Observable<User[]> = this.channelId$.pipe(
     switchMap((params) => {
@@ -29,15 +34,35 @@ export class ChannelComponent implements OnInit {
   constructor(
     private authService: AuthService,
     private route: ActivatedRoute,
-    private meetingService: MeetingService
-  ) {}
+    private snackBar: MatSnackBar,
+    public meetingService: MeetingService
+  ) {
+    // this.client.on('user-published', async (user, mediaType) => {
+    //   await this.client.subscribe(user, mediaType);
+    //   this.snackBar.open('参加者が増えました');
+    //   const remoreUserId = user.uid;
+    //   if (mediaType === 'video') {
+    //     const playerElement = document.createElement('div');
+    //     document.getElementById('remote-player-list').append(playerElement);
+    //     playerElement.outerHTML = `
+    //       <div id="player-wrapper-${remoreUserId}">
+    //         <p class="player-name">remoteUser(${remoreUserId})</p>
+    //         <div id="player-${remoreUserId}" class="player"></div>
+    //       </div>
+    //     `;
+    //     const remoteTrack = user.videoTrack;
+    //     remoteTrack.play('local-player');
+    //   }
+    //   if (mediaType === 'audio') {
+    //     user.audioTrack.play();
+    //   }
+    // });
+  }
 
   ngOnInit(): void {
-    console.log('channel');
     this.channelId$.subscribe((id) => {
       this.channelId = id;
     });
-    console.log(this.channelId);
   }
 
   async joinChannel(uid: string): Promise<void> {
@@ -48,5 +73,6 @@ export class ChannelComponent implements OnInit {
 
   async leaveChannel(uid: string): Promise<void> {
     this.meetingService.leaveChannel(uid, this.channelId);
+    this.players = false;
   }
 }
